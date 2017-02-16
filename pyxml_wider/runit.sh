140f8b636554350295f918eb236c1ed1baa27411
stream <penolove15@gmail.com>
2017-02-16 12:13:33 +0800

    add directory check and ImageSet creating

--- pyxml_wider/runit.sh
+++ pyxml_wider/runit.sh
@@ -1,8 +1,22 @@
+echo "check data setfolder exist" 
+
+DIRECTORY="../wider_face_split/"
+if [ ! -d "$DIRECTORY" ]; then
+    echo " Oops ,It seems data not downloaded properly !"
+    exit 1;
+fi
+
+DIRECTORY="../WIDER_train/"
+if [ ! -d "$DIRECTORY" ]; then
+    echo " Oops ,It seems data not downloaded properly !"
+    exit 1;
+fi
+
 echo "Createing xywhfile....."
 python mat_extract.py > xywhXfile.txt;
 
 echo "Since WIDER is a larger dataset, it may take for a while"
-echo "Createing WIDER2016 fold, Images , Annotation xmls..."
+echo "Createing WIDER_2016 fold, Images , Annotation xmls..."
 
 start=`date +%s`
 python anno2xml.py
@@ -13,4 +27,5 @@ runtime=$((end-start))
 rm xywhXfile.txt
 echo "totally it takes :" $runtime "seconds"
 
-
+#creating training testing set
+python randomSet.py WIDER_2016 0.9
